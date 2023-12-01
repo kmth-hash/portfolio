517c23b66101a4a367b881230858a774af8141b0
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-12-01 18:21:34 +0530

    Homepage #1 Added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,12 +3,17 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Homepage from './components/Homepage.jsx';
 import Header from './components/Header.jsx';
 import ErrorPage from './components/ErrorPage.jsx';
-
+import Projects from './components/Projects.jsx';
 
 function App() {
   let router = createBrowserRouter([{
     path : "/" , 
     element : <Homepage/>, 
+    errorElement : <ErrorPage/>
+  }, 
+  {
+    path : "/projects" , 
+    element : <Projects/>, 
     errorElement : <ErrorPage/>
   }, 
 ]);
