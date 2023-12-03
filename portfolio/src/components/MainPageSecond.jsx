b8259692bbdfbe0b5d3114a130c99b354544afe2
shareit.notfake@gmail.com <bpk.san4ya@gmail.com>
2023-12-03 23:50:14 +0530

    Styling changes in skill tags

--- portfolio/src/components/MainPageSecond.jsx
+++ portfolio/src/components/MainPageSecond.jsx
@@ -1,21 +1,26 @@
 import "../stylesheets/homepage.css";
-import { skills } from "../staticDataFile";
+import { skills, tools , colorPalette, textcolorPalette } from "../staticDataFile";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
 function MainPageSecond() {
   const [skillState, setSkillState] = useState(skills);
+  const [toolState , setToolState] = useState(tools);
+  
 
   return (
     <div className="row justify-content-between skills-main">
       <div className="col-12 mt-3 container ml-4 pl-4">
-        <h2 className="skillheader ">
-          &nbsp;<b>Professional Skills</b>
-        </h2>
+        
       </div>
       <div className="col-12 mt-3 container">
+
         <div className="row container">
+
           <div className="col-md-4 py-2 px-3">
+          <h2 className="skillheader ">
+          <b>Professional Skills</b>
+        </h2>
             <p>
               Focusing on writing efficient and clean code. Learning new tools
               to help build end to end products. Here are some languages,
@@ -23,6 +28,8 @@ function MainPageSecond() {
             </p>
           </div>
           <div className="col-md-8 p-2 ">
+            <h5><b>Languages</b></h5>
+            
             <div className="skill-grid d-flex flex-wrap gap-3">
               
               {skillState.map((skill, index) => {
@@ -32,7 +39,26 @@ function MainPageSecond() {
                   transition={{ type: "spring", stiffness: 400, damping: 10 }}
                   
                   >
-                    <div  className="text-center bg-dark-subtle fw-medium border border-2 border-dark p-2 rounded">
+                    <div style={{backgroundColor : colorPalette[index%4], color : textcolorPalette[index%4]}} className="text-center fw-medium border border-2 border-dark p-2 rounded">
+                    <i className={skill.tagLogo}></i>&nbsp;&nbsp;{skill.tagName} 
+                    
+                  </div>
+                  </motion.div>
+                );
+              })}
+            </div>
+            <br />
+            <h5><b>Tools</b></h5>
+            <div className="skill-grid d-flex flex-wrap gap-3">
+              
+              {toolState.map((skill, index) => {
+                return (
+                  <motion.div key={index} 
+                  whileHover={{ scale: 1.1 }}
+                  transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                  
+                  >
+                    <div style={{backgroundColor : colorPalette[index%4], color : textcolorPalette[index%4]}} className="text-center fw-medium border border-2 border-dark p-2 rounded">
                     <i className={skill.tagLogo}></i>&nbsp;&nbsp;{skill.tagName} 
                     
                   </div>
--- portfolio/src/staticDataFile.jsx
+++ portfolio/src/staticDataFile.jsx
@@ -26,16 +26,7 @@ const skillList = [
   {
     tagLogo: "devicon-html5-plain",
     tagName: "HTML",
-  },
-
-  {
-    tagLogo: "devicon-heroku-plain",
-    tagName: "Heroku",
-  },
-  {
-    tagLogo: "devicon-illustrator-plain",
-    tagName: "Illustrator",
-  },
+  },  
   {
     tagLogo: "devicon-java-plain",
     tagName: "Java",
@@ -44,14 +35,7 @@ const skillList = [
     tagLogo: "devicon-javascript-plain",
     tagName: "Javascript",
   },
-  {
-    tagLogo: "devicon-jenkins-plain",
-    tagName: "Jenkins",
-  },
-  {
-    tagLogo: "devicon-linux-plain",
-    tagName: "Linux",
-  },
+  
   {
     tagLogo: "devicon-mongodb-plain",
     tagName: "MongoDB",
@@ -60,14 +44,7 @@ const skillList = [
     tagLogo: "devicon-nodejs-plain",
     tagName: "NodeJS",
   },
-  {
-    tagLogo: "devicon-npm-original-wordmark",
-    tagName: "NPM",
-  },
-  {
-    tagLogo: "devicon-numpy-plain",
-    tagName: "NumPY",
-  },
+  
   {
     tagLogo: "devicon-php-plain",
     tagName: "PHP",
@@ -80,14 +57,42 @@ const skillList = [
     tagLogo: "devicon-python-plain",
     tagName: "Python",
   },
+  
+  {
+    tagLogo: "devicon-react-plain",
+    tagName: "React.JS",
+  },
+  
+
+];
+
+export const tools = [
+  {
+    tagLogo: "devicon-heroku-plain",
+    tagName: "Heroku",
+  },
+  {
+    tagLogo: "devicon-illustrator-plain",
+    tagName: "Illustrator",
+  },
   {
     tagLogo: "devicon-jenkins-plain",
     tagName: "Jenkins",
   },
   {
-    tagLogo: "devicon-react-plain",
-    tagName: "React.JS",
+    tagLogo: "devicon-linux-plain",
+    tagName: "Linux",
   },
+  {
+    tagLogo: "devicon-npm-original-wordmark",
+    tagName: "NPM",
+  },
+  {
+    tagLogo: "devicon-numpy-plain",
+    tagName: "NumPY",
+  },
+
+  
   {
     tagLogo: "devicon-selenium-plain",
     tagName: "Selenium",
@@ -96,10 +101,21 @@ const skillList = [
     tagLogo: "devicon-vscode-plain",
     tagName: "VSCode",
   }
-
 ];
 
+export const colorPalette = [
+  "#363062" , 
+  "#435585",
+  "#818FB4",
+  "#F5E8C7"
+] ; 
 
+export const textcolorPalette = [
+  "#FFFFFF" , 
+  "#FFFFFF" ,
+  "#FFFFFF" ,
+  "#000000" ,
 
+]
 
 export const skills = skillList;
